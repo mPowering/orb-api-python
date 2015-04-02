b93eea3b258d489eba527afb1910b141bff8db2c
alexlittle <alex@alexlittle.net>
2015-04-02 18:39:51 +0200

    getting initial version uploading and posting of resources working

--- examples/add_resource.py
+++ examples/add_resource.py
@@ -1,6 +1,6 @@
-
+#!/usr/bin/python
 import os, sys
-
+import MySQLdb
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_PATH = os.path.normpath(os.path.join(BASE_DIR, '..', 'mpower_api'))
@@ -8,7 +8,7 @@ PROJECT_PATH = os.path.normpath(os.path.join(BASE_DIR, '..', 'mpower_api'))
 if PROJECT_PATH not in sys.path:
     sys.path.append(PROJECT_PATH)
 
-from api import mpower_api
+from api import mpower_api, mpower_resource, mpower_resource_file, mpower_resource_url
 
 
 def run(): 
@@ -17,6 +17,30 @@ def run():
     api.user_name = 'demo'
     api.api_key = '39b4043c69b8db27ddba761ba82479d00c8ccbb1'
     
+    '''
+    db = MySQLdb.connect(host="localhost", # your host, usually localhost
+                     user="john", # your username
+                      passwd="megajonhy", # your password
+                      db="dc_") # name of the data base
+
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    cur = db.cursor() 
+    
+    # Use all the SQL you like
+    cur.execute("SELECT * FROM YOUR_TABLE_NAME")
+    
+    # print all the first cell of all the rows
+    for row in cur.fetchall() :
+        print row[0]
+    '''
+    
+    resource = mpower_resource()
+    resource.title = "Alex Test931"
+    resource.description = "something else to test with"
+    
+    id = api.add_resource(resource)
+    api.add_resource_image(id,'/home/alex/temp/image.jpg')
     
     
 if __name__ == "__main__":
--- mpower_api/api.py
+++ mpower_api/api.py
@@ -1,14 +1,17 @@
-import urllib2
+#!/usr/bin/python
 import json
+import urllib2
+
+from poster.encode import multipart_encode
+from poster.streaminghttp import register_openers
+
 
 class mpower_api():
     base_url = ''
     user_name = ''
     api_key = ''
     
-    
     def search(self, query):
-        
         req = urllib2.Request(self.base_url + '/api/v1/resource/search/?q='+query)
         req.add_header('Authorization', 'ApiKey '+self.user_name + ":" + self.api_key)
         resp = urllib2.urlopen(req)
@@ -16,9 +19,72 @@ class mpower_api():
         dataJSON = json.loads(data)
         return dataJSON
     
+    def add_resource(self,resource):
+        data = json.dumps({'title': resource.title, 'description': resource.description })
+        
+        # make a string with the request type in it:
+        method = "POST"
+        # create a handler. you can specify different handlers here (file uploads etc)
+        # but we go for the default
+        handler = urllib2.HTTPHandler()
+        # create an openerdirector instance
+        opener = urllib2.build_opener(handler)
+        # build a request
+        request = urllib2.Request(self.base_url + '/api/v1/resource/', data=data )
+        # add any other information you want
+        request.add_header("Content-Type",'application/json')
+        request.add_header('Authorization', 'ApiKey '+self.user_name + ":" + self.api_key)
+        # overload the get method function with a small anonymous function...
+        request.get_method = lambda: method
+        # try it; don't forget to catch the result
+        try:
+            connection = opener.open(request)
+        except urllib2.HTTPError,e:
+            connection = e
+            
+        if connection.code == 401:
+            # unauthorised
+            pass
+        elif connection.code == 400:
+            # already exists or other error
+            pass
+        elif connection.code == 201:
+            # already exists or other error
+            resp = connection.read()
+            data_json = json.loads(resp)
+            return data_json['id']
+            
     
-    
-    def send(self):
         return
+    
+    def add_resource_image(self, resource_id, image_file):
+        register_openers()
+        
+        datagen, headers = multipart_encode({'resource_id': resource_id, 'image_file': open(image_file)})
+        handler = urllib2.HTTPHandler()
+        request = urllib2.Request(self.base_url + '/api/upload/image/', datagen, headers )
+        request.add_header('Authorization', 'ApiKey '+self.user_name + ":" + self.api_key)
+        
+        resp = urllib2.urlopen(request).read()
+        print resp
+        
+        return
+    
+ 
+class mpower_resource():
+    title = ''
+    description = ''
+    
+class mpower_resource_file():
+    file = ''
+    title = ''
+    description = ''
+    
+class mpower_resource_url():
+    url = ''
+    title = ''
+    description = ''
+    
+       
     
     
